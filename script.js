d7cc315e6f88ba073627ecb77bd8d6dddf9e8920
JQ4386 <jingqi_qu@berkeley.edu>
2023-06-05 18:43:01 +1000

    getComputerChoice() done

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+// Example usage
+const randomChoice = getComputerChoice();
+console.log(randomChoice);
